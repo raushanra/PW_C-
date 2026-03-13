a66fcacda144c19c9ca717c412771a4809b12ac6
raushanra <raushan001iit@gmail.com>
2026-03-13 11:54:03 +0530

    Arithmetic_Operator

---
+++ Arithmetic_operators.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num1;
+    int num2;
+    
+    cout<<"num1 :"<<endl;
+    cin>>num1;
+    cout<<"num2:"<<endl;
+    cin>>num2;
+   cout<<num1+num2<<endl;
+   cout<<num1-num2<<endl;
+   cout<<num1*num2<<endl;
+   cout<<num1/num2<<endl;
+   cout<<num1%num2<<endl;
+    
+    return 0;
+    
+}
